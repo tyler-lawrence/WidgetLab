c140b970d56ab5edb9ab8a0a56c27608cfabe680
Tyler Lawrence <tyler.lawrence15@gmail.com>
2025-03-04 11:18:01 -0500

    Initial Commit

---
+++ CoffeeCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeeCounter
+//
+//  Created by Tyler Lawrence1 on 3/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
