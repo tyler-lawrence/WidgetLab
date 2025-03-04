bd5efa29bd7c85da9911d76cc8254a9d02f5addf
Tyler Lawrence <tyler.lawrence15@gmail.com>
2025-03-04 11:18:01 -0500

    Initial Commit

---
+++ CoffeeCounter/CoffeeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeCounterApp.swift
+//  CoffeeCounter
+//
+//  Created by Tyler Lawrence1 on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
