a4ebaad5c28045ec686fb9797a7344aa031a1cfe
Tyler Lawrence <tyler.lawrence15@gmail.com>
2025-03-06 11:26:46 -0500

    deleted warning

--- CoffeeCounter/DataController.swift
+++ CoffeeCounter/DataController.swift
@@ -12,7 +12,6 @@ import WidgetKit
 @Observable
 class DataController {
     // While force unwrapping generally isn't preferred, I don't mind it in this conxext because it's signaling to me as the developer that something is wrong with my data storage, which is an important feature of this app
-    #warning("1.3 - Add your app group name here")
     let defaults = UserDefaults(suiteName: "group.com.academy.CoffeeCounter")!
     var coffeeCount: Int {
         /*
